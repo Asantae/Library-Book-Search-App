94d80051ed08cf30b8847b887b94c01b991ad971
Asantae Shelton <64320195+Asantae@users.noreply.github.com>
2022-03-29 23:05:39 -0400

    init commit

---
+++ App.js
@@ -0,0 +1,3 @@
+
+const API_KEY = 'https://language.googleapis.com/v1/documents:analyzeEntities?key=AIzaSyCdEMmaKOke-OQ3TmC-rMGMdoXS0wATjrQ';
+console.log(API_KEY);
---
+++ key.js
@@ -0,0 +1,4 @@
+// google-books api key AIzaSyCdEMmaKOke-OQ3TmC-rMGMdoXS0wATjrQ
+
+
+//API_KEY = 'https://language.googleapis.com/v1/documents:analyzeEntities?key=AIzaSyCdEMmaKOke-OQ3TmC-rMGMdoXS0wATjrQ';
